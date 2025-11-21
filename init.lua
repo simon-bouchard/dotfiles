bcfaefcb2685aee5aa7432ec5e030bc855e3705e
Simon Bouchard <simon.bouchard31@gmail.com>
2025-11-21 02:10:38 -0500

    fix neovim setup and add lsp

--- init.lua
+++ init.lua
@@ -5,32 +5,95 @@ require("lazy").setup({
   -- UI & Appearance
   "nvim-lualine/lualine.nvim",
   "folke/tokyonight.nvim",
-  
-  -- Treesitter (better syntax highlighting)
+  "morhetz/gruvbox",
+  "dracula/vim",
+  "arcticicestudio/nord-vim",
+  "joshdick/onedark.vim",
+  "rose-pine/neovim",
+
+  -- Treesitter
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
   },
-  
-  -- Telescope (fuzzy finder)
+
+  -- Telescope
   "nvim-telescope/telescope.nvim",
   "nvim-lua/plenary.nvim",
-  
-  -- LSP Setup
+
+  -- LSP & Mason
   "neovim/nvim-lspconfig",
   {
     "williamboman/mason.nvim",
-    build = ":MasonUpdate",
+    config = function()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+          }
+        }
+      })
+    end
   },
-  "williamboman/mason-lspconfig.nvim",
-  
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "pylsp",
+          "lua_ls",
+          "ts_ls",
+          "html",
+          "cssls",
+          "bashls",
+          "jsonls",
+          "yamlls",
+        },
+        automatic_installation = true,
+      })
+    end
+  },
+
   -- Autocompletion
-  "hrsh7th/nvim-cmp",
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      local cmp = require("cmp")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+        }, {
+          { name = 'buffer' },
+        })
+      })
+    end
+  },
   "hrsh7th/cmp-nvim-lsp",
   "hrsh7th/cmp-buffer",
   "hrsh7th/cmp-path",
   "L3MON4D3/LuaSnip",
-  
+
   -- Linting
   {
     "mfussenegger/nvim-lint",
@@ -42,9 +105,16 @@ require("lazy").setup({
         bash = {"shellcheck"},
         zsh = {"shellcheck"},
         python = {"pylint"},
+        javascript = {"eslint"},
+        typescript = {"eslint"},
       }
 
-      -- Run lint automatically on write
+      -- Disable pylint whitespace warnings
+      lint.linters.pylint.args = {
+        "--disable=W391",  -- blank line at end of file
+        "--disable=C0111", -- missing docstring
+      }
+
       vim.api.nvim_create_autocmd("BufWritePost", {
         callback = function()
           lint.try_lint()
@@ -55,129 +125,53 @@ require("lazy").setup({
 })
 
 -- ============================================================================
--- LSP SETUP WITH MASON
--- ============================================================================
-local mason = require("mason")
-local mason_lspconfig = require("mason-lspconfig")
-local lspconfig = require("lspconfig")
-local cmp = require("cmp")
-
--- Mason configuration
-mason.setup({
-  ui = {
-    icons = {
-      package_installed = "✓",
-      package_pending = "➜",
-      package_uninstalled = "✗"
-    }
-  }
-})
-
--- LSP servers to automatically install
-mason_lspconfig.setup({
-  ensure_installed = {
-    "lua_ls",           -- Lua
-    "pylsp",            -- Python (python-lsp-server)
-    -- Add more servers as needed:
-    -- "ts_ls",          -- TypeScript/JavaScript
-    -- "bashls",         -- Bash
-    -- "marksman",       -- Markdown
-    -- "jsonls",         -- JSON
-  },
-  automatic_installation = true,
-})
-
--- Autocompletion setup
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  }, {
-    { name = 'buffer' },
-  })
-})
-
--- LSP server configuration
-local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  
-  -- Keymaps
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', '<leader>f', function()
-    vim.lsp.buf.format { async = true }
-  end, bufopts)
-end
-
--- Apply on_attach and capabilities to each server
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-mason_lspconfig.setup_handlers({
-  function(server_name)
-    lspconfig[server_name].setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
-    })
-  end,
-})
-
--- ============================================================================
 -- UI SETTINGS
 -- ============================================================================
 
--- Make sure parsers are found or compiled
 vim.opt.runtimepath:append(vim.fn.stdpath("data") .. "/lazy/parsers")
 
--- LEADER KEY
 vim.g.mapleader = " "
 
--- UI
-vim.opt.number = true                  -- absolute line numbers
-vim.opt.relativenumber = true          -- relative line numbers
-vim.opt.laststatus = 3                 -- global status line
-vim.opt.cursorline = true              -- highlight current line
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.laststatus = 3
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
 
--- SEARCH
-vim.opt.ignorecase = true              -- case-insensitive search
-vim.opt.smartcase = true               -- unless uppercase present
-vim.opt.incsearch = true               -- search while typing
-vim.opt.hlsearch = true                -- highlight search results
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
 
--- TABS & INDENTATION
-vim.opt.expandtab = true               -- convert tabs to spaces
-vim.opt.tabstop = 4                    -- 1 tab = 4 spaces
-vim.opt.shiftwidth = 4                 -- indentation amount
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.backspace = { "indent", "eol", "start" }
 
--- CLIPBOARD
-vim.opt.clipboard = "unnamed"          -- use system clipboard
+vim.opt.clipboard = "unnamed"
+vim.opt.mouse = "a"
 
--- MOUSE
-vim.opt.mouse = "a"                    -- enable mouse
+-- Strip trailing whitespace on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    local save_cursor = vim.fn.getpos(".")
+    vim.cmd([[%s/\s\+$//e]])
+    vim.fn.setpos(".", save_cursor)
+  end,
+})
 
--- COLOR SCHEME
 vim.cmd.colorscheme "tokyonight"
+-- Other options: "gruvbox", "dracula", "nord", "onedark", "rose-pine"
+-- Change above line to try different themes
 
--- ARROW-KEY DISABLING
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+})
+
 vim.keymap.set("n", "<Left>",  ":echo 'Use h'<CR>")
 vim.keymap.set("n", "<Right>", ":echo 'Use l'<CR>")
 vim.keymap.set("n", "<Up>",    ":echo 'Use k'<CR>")
@@ -188,16 +182,70 @@ vim.keymap.set("i", "<Right>", "<ESC>:echo 'Use l'<CR>")
 vim.keymap.set("i", "<Up>",    "<ESC>:echo 'Use k'<CR>")
 vim.keymap.set("i", "<Down>",  "<ESC>:echo 'Use j'<CR>")
 
--- PASTE MODE TOGGLE
 vim.keymap.set("n", "<leader>p", function()
   vim.opt.paste = not vim.opt.paste:get()
   print("paste = " .. (vim.opt.paste:get() and "ON" or "OFF"))
 end)
 
--- LUALINE SETUP
 require('lualine').setup({
   options = {
     icons_enabled = true,
     theme = 'tokyonight',
   }
+})
+
+-- ============================================================================
+-- LSP HANDLERS (runs after all plugins load)
+-- ============================================================================
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+    local on_attach = function(client, bufnr)
+      local bufopts = { noremap=true, silent=true, buffer=bufnr }
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+      vim.keymap.set('n', '<leader>f', function()
+        vim.lsp.buf.format { async = true }
+      end, bufopts)
+      vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, bufopts)
+      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, bufopts)
+      vim.keymap.set('n', ']d', vim.diagnostic.goto_next, bufopts)
+    end
+
+    local capabilities = cmp_nvim_lsp.default_capabilities()
+
+    local default_config = {
+      on_attach = on_attach,
+      capabilities = capabilities,
+    }
+
+    -- Setup each LSP server using vim.lsp.config
+    local servers = {
+      { name = "pylsp", cmd = { "pylsp" } },
+      { name = "lua_ls", cmd = { "lua-language-server" } },
+      { name = "ts_ls", cmd = { "typescript-language-server", "--stdio" } },
+      { name = "html", cmd = { "vscode-html-language-server", "--stdio" } },
+      { name = "cssls", cmd = { "vscode-css-language-server", "--stdio" } },
+      { name = "bashls", cmd = { "bash-language-server", "start" } },
+      { name = "jsonls", cmd = { "vscode-json-language-server", "--stdio" } },
+      { name = "yamlls", cmd = { "yaml-language-server", "--stdio" } },
+    }
+
+    for _, server in ipairs(servers) do
+      vim.lsp.config(server.name, {
+        cmd = server.cmd,
+        root_markers = { ".git" },
+        on_attach = default_config.on_attach,
+        capabilities = default_config.capabilities,
+      })
+      vim.lsp.enable(server.name)
+    end
+  end,
 })
