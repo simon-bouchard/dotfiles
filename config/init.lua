0236fa6ee910cad9fa4fd4db758214ada31bf67b
Simon Bouchard <simon.bouchard31@gmail.com>
2026-05-04 00:32:42 -0400

    add clang to lsp server

--- config/init.lua
+++ config/init.lua
@@ -55,6 +55,7 @@ require("lazy").setup({
                     "bashls",
                     "jsonls",
                     "yamlls",
+                    "clangd",
                 },
                 automatic_installation = true,
             })
@@ -294,6 +295,7 @@ vim.api.nvim_create_autocmd("User", {
             { name = "bashls", cmd = { "bash-language-server", "start" } },
             { name = "jsonls", cmd = { "vscode-json-language-server", "--stdio" } },
             { name = "yamlls", cmd = { "yaml-language-server", "--stdio" } },
+            { name = "clangd", cmd = { "clangd" } },
         }
 
         for _, server in ipairs(servers) do
