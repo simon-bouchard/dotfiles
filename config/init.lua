b5be04bb8780df0b2e21a45a6043161db4f06ee8
Simon Bouchard <simon.bouchard31@gmail.com>
2026-07-18 17:40:54 -0400

    add telescope (fzf) keybinds to nvim config

--- config/init.lua
+++ config/init.lua
@@ -1,3 +1,5 @@
+vim.g.mapleader = " "
+
 -- lazy.nvim bootstrap
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -26,8 +28,32 @@ require("lazy").setup({
     },
 
     -- Telescope
-    "nvim-telescope/telescope.nvim",
-    "nvim-lua/plenary.nvim",
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "make",
+            },
+        },
+        config = function()
+            local telescope = require("telescope")
+            telescope.setup({
+                defaults = {
+                    file_ignore_patterns = { "%.git/" },
+                },
+            })
+            telescope.load_extension("fzf")
+
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Grep files" })
+            vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
+            vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+            vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Recent files" })
+        end,
+    },
 
     -- LSP & Mason
     "neovim/nvim-lspconfig",
@@ -182,8 +208,6 @@ require("lazy").setup({
 -- ============================================================================
 
 vim.opt.runtimepath:append(vim.fn.stdpath("data") .. "/lazy/parsers")
-
-vim.g.mapleader = " "
 
 vim.opt.number = true
 vim.opt.relativenumber = true
